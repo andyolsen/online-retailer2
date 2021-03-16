adb69f34f093ae0d137b3750ab6cbbd4e6a4bdff
Andy Olsen <andyo@olsensoft.com>
2021-03-16 07:02:55 +0000

    Update ProductRepository.java

--- src/main/java/no/dnb/reskill/onlineretailer/ProductRepository.java
+++ src/main/java/no/dnb/reskill/onlineretailer/ProductRepository.java
@@ -3,7 +3,12 @@ package no.dnb.reskill.onlineretailer;
 import java.util.Collection;
 
 public interface ProductRepository {
+    
+    // FROM ANDY: Should return the product id, or maybe a whole Product object with id.
     void addProduct(Product product);
+    
+    // FROM ANDY: Could also have an updateProduct method.
+    
     boolean deleteProduct(int productId);
     Collection<Product> findAllProducts();
 }
