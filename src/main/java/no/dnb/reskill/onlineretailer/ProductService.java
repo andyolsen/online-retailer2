09278f6b62f6929195a758e576c75ef94b9744dc
Andy Olsen <andyo@olsensoft.com>
2021-03-16 07:06:03 +0000

    Update ProductService.java

--- src/main/java/no/dnb/reskill/onlineretailer/ProductService.java
+++ src/main/java/no/dnb/reskill/onlineretailer/ProductService.java
@@ -4,5 +4,7 @@ public interface ProductService {
     void addToStock(Product product);
     void deleteFromStock(int id);
     Product findInStock(int productId);
+    
+    // FROM ANDY: The productName might not be unique, so maybe this method should return a collection?
     Product findInStock(String productName);
 }
